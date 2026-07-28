be4b3cfaad9e58758246de64e1198a9fdf97404f
abdkad123 <akaddoura123@gmail.com>
2026-07-28 04:01:15 +0300

    react

--- ecommerse/src/pages/HomePage.jsx
+++ ecommerse/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ export function HomePage() {
     return (
         <>
             <title>Ecommerse</title>
+            <link rel="icon" type="image/svg+xml" href="images/home-favicon.png" />
             <Header />
             
             <div className="home-page">
---
+++ ecommerse/src/pages/Unknown.jsx
@@ -0,0 +1,5 @@
+export function Unknown() {
+    return (
+        <p>Page not fount 404</p>
+    );
+}
