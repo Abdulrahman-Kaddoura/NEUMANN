8488a0229dc63ba30b89df67b2a19282127edabb
abdkad123 <akaddoura123@gmail.com>
2026-07-29 20:05:41 +0300

    async await

--- ecommerce/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce/src/pages/checkout/CheckoutPage.jsx
@@ -7,12 +7,19 @@ import './CheckoutPage.css'
 
 export function CheckoutPage({ cart }) {
     const [deliveryOptions, setDeliveryOptions] = useState([]);
+    const [paymentSummary, setPaymentSummary] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
-            .then((response) => {
-                setDeliveryOptions(response.data);
-            });
+        const getDeliveryOptions = async () => {
+            const response = axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+            setDeliveryOptions(response.data);
+        }
+
+        getDeliveryOptions();
+
+        axios.get('/api/payment-summary').then((response) => {
+            setPaymentSummary(response.data);
+        });
     }, []);
 
     return (
