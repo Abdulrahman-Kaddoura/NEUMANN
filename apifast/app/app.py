006b4a195126c386fcaf260d57f1fbd4232c0eb3
abdkad123 <akaddoura123@gmail.com>
2026-07-31 23:57:42 +0300

    trying out dif requests

--- apifast/app/app.py
+++ apifast/app/app.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/hello-world")
-def hello_world():
-    return {"message": "Hello World"}
+text_posts = {1: {"text": "hello"}, 2: {"text": "hi"}}
+
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    return text_posts.get(id)
