85de73814e6b296fe883030d3f42be466a5f9e47
abdkad123 <akaddoura123@gmail.com>
2026-08-01 22:37:06 +0300

    more fastapi

--- apifast/app/app.py
+++ apifast/app/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse
 from app.db import Post, create_db_and_tables, get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,35 +11,11 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-text_posts = {
-    1: {"text": "hello"},
-    2: {"text": "hi"},
-    3: {"text": "good morning"},
-    4: {"text": "what's up"},
-    5: {"text": "fastapi is fun"},
-    6: {"text": "learning uv today"},
-    7: {"text": "postgres setup done"},
-    8: {"text": "async is tricky"},
-    9: {"text": "almost done with the api"},
-    10: {"text": "goodnight"},
-}
 
-@app.get("/posts")
-def get_all_posts(limit: int = None):
-    if limit:
-        return dict(list(text_posts.items())[:limit])
-    return text_posts
-
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    if id not in text_posts:
-        return HTTPException(status_code=404, detail="Post not found")
-    return text_posts.get(id)
-
-@app.post("/posts")
-def create_post(post: PostCreate) -> PostResponse:
-    new_post = {"text": post.text}
-    text_posts[max(text_posts.keys()) + 1] = new_post
-    return new_post
-
+@app.post("/upload")
+async def upload_file(
+    file: UploadFile = File(...),
+    caption: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)
+): 
+    pass
