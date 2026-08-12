28f67bb2fe362f280affaf89f25ec7b59875ccd7
abdkad123 <akaddoura123@gmail.com>
2026-08-13 00:24:26 +0300

    styling both forms

--- frontend/src/components/employee/AddEmployeeForm.tsx
+++ frontend/src/components/employee/AddEmployeeForm.tsx
@@ -14,51 +14,53 @@ export function AddEmployeeForm({ setAddFormVisible }: AddEmployeeFormProps) {
                     <p>Fill in the details below to add a new employee.</p>
                 </div>
 
-                <div className='form-group'>
-                    <label htmlFor='first-name'>First Name</label>
-                    <input className='form-input' required id='first-name' placeholder='Enter First Name' />
-                </div>
+                <div className='add-form-fields'>
+                    <div className='form-group'>
+                        <label htmlFor='first-name'>First Name</label>
+                        <input className='form-input' required id='first-name' placeholder='Enter First Name' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='last-name'>Last Name</label>
-                    <input className='form-input' required id='last-name' placeholder='Enter Last Name' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='last-name'>Last Name</label>
+                        <input className='form-input' required id='last-name' placeholder='Enter Last Name' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='company'>Company</label>
-                    <select className='form-input' required id='company'>
-                        <option value='Benton'>Benton</option>
-                        <option value='Chanay'>Chanay</option>
-                        <option value='Chemel'>Chemel</option>
-                        <option value='Feltz Printing'>Feltz Printing</option>
-                        <option value='Commercial Press'>Commercial Press</option>
-                        <option value='Yummy'>Yummy</option>
-                    </select>
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='company'>Company</label>
+                        <select className='form-input' required id='company'>
+                            <option value='Benton'>Benton</option>
+                            <option value='Chanay'>Chanay</option>
+                            <option value='Chemel'>Chemel</option>
+                            <option value='Feltz Printing'>Feltz Printing</option>
+                            <option value='Commercial Press'>Commercial Press</option>
+                            <option value='Yummy'>Yummy</option>
+                        </select>
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='job-title'>Job Title</label>
-                    <input className='form-input' required id='job-title' placeholder='Enter Job Title' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='job-title'>Job Title</label>
+                        <input className='form-input' required id='job-title' placeholder='Enter Job Title' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='email'>Email</label>
-                    <input className='form-input' id='email' type='email' placeholder='Enter Email' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='email'>Email</label>
+                        <input className='form-input' id='email' type='email' placeholder='Enter Email' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='address'>Address</label>
-                    <input className='form-input' required id='address' placeholder='Enter Address' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='address'>Address</label>
+                        <input className='form-input' required id='address' placeholder='Enter Address' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='city'>City</label>
-                    <input className='form-input' required id='city' placeholder='Enter City' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='city'>City</label>
+                        <input className='form-input' required id='city' placeholder='Enter City' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='county'>County</label>
-                    <input className='form-input' required id='county' placeholder='Enter County' />
+                    <div className='form-group'>
+                        <label htmlFor='county'>County</label>
+                        <input className='form-input' required id='county' placeholder='Enter County' />
+                    </div>
                 </div>
 
                 <div className='add-form-buttons'>
--- frontend/src/components/employee/EditEmployeeForm.tsx
+++ frontend/src/components/employee/EditEmployeeForm.tsx
@@ -33,51 +33,53 @@ export function EditEmployeeForm({ setEditFormVisible,
 
                 <input type='hidden' id='employeeId' />
 
-                <div className='form-group'>
-                    <label htmlFor='first-name'>First Name</label>
-                    <input className='form-input' value={firstName} required id='first-name' placeholder='Enter First Name' />
-                </div>
+                <div className='add-form-fields'>
+                    <div className='form-group'>
+                        <label htmlFor='first-name'>First Name</label>
+                        <input className='form-input' value={firstName} required id='first-name' placeholder='Enter First Name' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='last-name'>Last Name</label>
-                    <input className='form-input' value={lastName} required id='last-name' placeholder='Enter Last Name' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='last-name'>Last Name</label>
+                        <input className='form-input' value={lastName} required id='last-name' placeholder='Enter Last Name' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='company'>Company</label>
-                    <select className='form-input' value={company} required id='company'>
-                        <option value='Benton'>Benton</option>
-                        <option value='Chanay'>Chanay</option>
-                        <option value='Chemel'>Chemel</option>
-                        <option value='Feltz Printing'>Feltz Printing</option>
-                        <option value='Commercial Press'>Commercial Press</option>
-                        <option value='Yummy'>Yummy</option>
-                    </select>
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='company'>Company</label>
+                        <select className='form-input' value={company} required id='company'>
+                            <option value='Benton'>Benton</option>
+                            <option value='Chanay'>Chanay</option>
+                            <option value='Chemel'>Chemel</option>
+                            <option value='Feltz Printing'>Feltz Printing</option>
+                            <option value='Commercial Press'>Commercial Press</option>
+                            <option value='Yummy'>Yummy</option>
+                        </select>
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='job-title'>Job Title</label>
-                    <input className='form-input' value={jobTitle} required id='job-title' placeholder='Enter Job Title' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='job-title'>Job Title</label>
+                        <input className='form-input' value={jobTitle} required id='job-title' placeholder='Enter Job Title' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='email'>Email</label>
-                    <input className='form-input' value={email} id='email' type='email' placeholder='Enter Email' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='email'>Email</label>
+                        <input className='form-input' value={email} id='email' type='email' placeholder='Enter Email' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='address'>Address</label>
-                    <input className='form-input' required value={address} id='address' placeholder='Enter Address' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='address'>Address</label>
+                        <input className='form-input' required value={address} id='address' placeholder='Enter Address' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='city'>City</label>
-                    <input className='form-input' required value={city} id='city' placeholder='Enter City' />
-                </div>
+                    <div className='form-group'>
+                        <label htmlFor='city'>City</label>
+                        <input className='form-input' required value={city} id='city' placeholder='Enter City' />
+                    </div>
 
-                <div className='form-group'>
-                    <label htmlFor='county'>County</label>
-                    <input className='form-input' required value={county} id='county' placeholder='Enter County' />
+                    <div className='form-group'>
+                        <label htmlFor='county'>County</label>
+                        <input className='form-input' required value={county} id='county' placeholder='Enter County' />
+                    </div>
                 </div>
 
                 <div className='add-form-buttons'>
