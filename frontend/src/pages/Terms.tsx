ba4469a42c32f017cdfe2bfe4b86b3b76833c839
abdkad123 <akaddoura123@gmail.com>
2026-08-31 21:04:07 +0300

    make back to sign up close the tab and remove app.css

--- frontend/src/pages/Terms.tsx
+++ frontend/src/pages/Terms.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router";
+import type { MouseEvent } from "react";
 
 export function Terms() {
+  function handleBackToSignUp(event: MouseEvent<HTMLAnchorElement>) {
+    event.preventDefault();
+    window.close();
+  }
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
       <title>Terms</title>
       <h1 style={{ margin: 0 }}>Sample terms and conditions of a website</h1>
-      <Link to="/signup">Back to sign up</Link>
+      <Link to="/signup" onClick={handleBackToSignUp}>Back to sign up</Link>
     </div>
   );
 }
